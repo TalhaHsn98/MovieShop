b1a31aefedaf8c5c9b4442f2311b64eff6118a5b
TalhaHsn98 <syedtalha.hussain98@gmail.com>
2025-08-26 19:02:35 -0400

    Initial Commit

---
+++ Infrastructure/Repositories/CastRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationCore.Contracts.Repository;
+
+namespace Infrastructure.Repositories
+{
+    internal class CastRepository : ICastRepository
+    {
+       
+    }
+}
